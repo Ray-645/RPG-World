12bd45fea840beb25e90a43b6ec5c64a54d05e1f
Raymon S <136154556+Ray-645@users.noreply.github.com>
2023-06-25 15:37:30 -0400

    Added Alpha Ver. 0.01

---
+++ Alpha/Ver 0.01/RPG World Base.py
@@ -0,0 +1,32 @@
+# RPG World
+# Ver. 0.01
+# 06-25-2023
+
+"""____________________  ________   __      __            .__       .___
+\______   \______   \/  _____/  /  \    /  \___________|  |    __| _/
+ |       _/|     ___/   \  ___  \   \/\/   /  _ \_  __ \  |   / __ | 
+ |    |   \|    |   \    \_\  \  \        (  <_> )  | \/  |__/ /_/ | 
+ |____|_  /|____|    \______  /   \__/\  / \____/|__|  |____/\____ | 
+        \/                  \/         \/                         \/ 
+"""
+import time
+import os 
+
+print("                                                   Created By Raymon Shields                    ")
+
+time.sleep(3)
+os.system('cls')
+
+print("                                                     WELCOME TO RPG WORLD                    ")
+print("                                            A text based RPG Game made in Python 3.6                    ")
+print("Ver.0.01")
+print("By Raymon Shields")
+
+
+from playsound import playsound
+
+playsound ('.Assets\Earthbound\eb_fanfare.wav')
+
+print("'Press Enter To Continue'")
+
+input1 = input()
