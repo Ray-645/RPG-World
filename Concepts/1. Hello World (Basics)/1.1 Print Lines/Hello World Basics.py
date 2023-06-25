b57d466774539e644d673d89eed68ba790ebaa91
Raymon S <136154556+Ray-645@users.noreply.github.com>
2023-06-25 12:55:59 -0400

    Added my first Python File

---
+++ Concepts/1. Hello World (Basics)/1.1 Print Lines/Hello World Basics.py
@@ -0,0 +1,13 @@
+
+
+# The Basic's Of Programmming
+
+print ("Hello  World")
+
+# Testing Testing One Two Three...
+
+print ("This is a message from the stars")
+
+exit() #End The Program
+
+# (6-25-2023) By Raymon Shields 
